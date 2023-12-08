48643f94c7472e8853ec230d1dd9b40da02944b7
James Privett <james.privett@feefo.com>
2023-12-08 10:51:02 +0000

    LoginForm initial implementation

--- registration.ui/src/App.tsx
+++ registration.ui/src/App.tsx
@@ -3,6 +3,7 @@ import { getUsers, registerUser } from './Api';
 import './App.css';
 import RegisterForm from './components/RegisterForm';
 import { UserDTO } from './Types';
+import LoginForm from './components/LoginForm';
 
 const App = () => {
   // useEffect(() => {
@@ -12,11 +13,17 @@ const App = () => {
   const [registerEmailValue, setRegisterEmailValue] = useState('');
   const [registerPasswordValue, setRegisterPasswordValue] = useState('');
 
+  const [loginUsernameValue, setLoginUsernameValue] = useState('');
+  const [loginPasswordValue, setLoginPasswordValue] = useState('');
+
   //const [userDTO, setUserDto] = useState<UserDTO>();
 
   const handleRegisterUsernameValue = (enteredText: string) => setRegisterUsernameValue(enteredText);
   const handleRegisterEmailValue = (enteredText: string) => setRegisterEmailValue(enteredText);
   const handleRegisterPasswordValue = (enteredText: string) => setRegisterPasswordValue(enteredText);
+
+  const handleLoginUsernameValue = (enteredText: string) => setLoginUsernameValue(enteredText);
+  const handleLoginPasswordValue = (enteredText: string) => setLoginPasswordValue(enteredText);
 
 
   const handleRegisterOnSubmit = async (event: any, user: UserDTO) => {
@@ -24,17 +31,37 @@ const App = () => {
     console.log(user);
     await registerUser(user);
   }
+
+  const handleLoginSubmit = async (event: any, username: string, password: string) => {
+    event.preventDefault();
+    console.log('boom')
+  }
+
   return (
     <main className="App">
-      <RegisterForm
-        usernameValue={registerUsernameValue}
-        emailValue={registerEmailValue}
-        passwordValue={registerPasswordValue}
-        handleUsernameValue={handleRegisterUsernameValue}
-        handleEmailValue={handleRegisterEmailValue}
-        handlePasswordValue={handleRegisterPasswordValue}
-        handleOnSubmit={handleRegisterOnSubmit}
-      />
+      <section>
+        <RegisterForm
+          usernameValue={registerUsernameValue}
+          emailValue={registerEmailValue}
+          passwordValue={registerPasswordValue}
+          handleUsernameValue={handleRegisterUsernameValue}
+          handleEmailValue={handleRegisterEmailValue}
+          handlePasswordValue={handleRegisterPasswordValue}
+          handleOnSubmit={handleRegisterOnSubmit}
+        />
+      </section>
+      <section>
+        <LoginForm
+          usernameValue={loginUsernameValue}
+          passwordValue={loginPasswordValue}
+          handleUsernameValue={handleLoginUsernameValue}
+          handlePasswordValue={handleLoginPasswordValue}
+          handleOnSubmit={handleLoginSubmit}
+        />
+      </section>
+      <section>
+        <article></article>
+      </section>
     </main>
   );
 }
