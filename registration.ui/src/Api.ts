d64089cf6f15ff62e01768ccd580435629edd90f
James Privett <james.privett@feefo.com>
2023-12-08 09:15:34 +0000

    registerUser API call added

--- registration.ui/src/Api.ts
+++ registration.ui/src/Api.ts
@@ -1,14 +1,34 @@
+import { UserDTO } from "./Types";
+
 const DOMAIN = 'https://localhost:7185';
 
 export const getUsers = async () => {
     await fetch(`${DOMAIN}/users`, {
         method: 'GET',
         mode: 'cors'
-      }).then((response) => {
+    }).then((response) => {
         if (!response.ok) {
-          throw new Error(JSON.stringify(response.json()))
-      }
+            throw new Error(JSON.stringify(response.json()))
+        }
 
-      return response.json();
-      });
+        return response.json();
+    });
+}
+
+export const registerUser = async (user: UserDTO) => {
+    console.log(user);
+    await fetch(`${DOMAIN}/register`, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+    }).then((response) => {
+        if (!response.ok) {
+            throw new Error(JSON.stringify(response.json()))
+        }
+
+        return response.json();
+    });
 }
