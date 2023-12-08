f326ad161409c6da43ab6e456c2583402e38e505
James Privett <james.privett@feefo.com>
2023-12-08 09:15:57 +0000

    Register User working

--- registration.ui/src/App.tsx
+++ registration.ui/src/App.tsx
@@ -1,15 +1,23 @@
-import React, { useEffect } from 'react';
-import { getUsers } from './Api';
+import React, { useEffect, useState } from 'react';
+import { getUsers, registerUser } from './Api';
 import './App.css';
 import RegisterForm from './components/RegisterForm';
+import { UserDTO } from './Types';
 
 const App = () => {
-  useEffect(() => {
-    getUsers();
-  }, []);
+  // useEffect(() => {
+  //   getUsers();
+  // }, []);
+  const [userDTO, setUserDto] = useState<UserDTO>();
+
+  const handleRegisterOnSubmit = (event: any, user: UserDTO) => {
+    event.preventDefault();
+    console.log(user);
+    registerUser(user);
+  }
   return (
     <main className="App">
-      <RegisterForm />
+      <RegisterForm handleOnSubmit={handleRegisterOnSubmit} />
     </main>
   );
 }
--- registration.ui/src/components/RegisterForm.tsx
+++ registration.ui/src/components/RegisterForm.tsx
@@ -1,36 +1,65 @@
+import { useState } from "react";
 import Button, { ButtonType } from "./shared/Button";
 import Label from "./shared/Label";
-import TextInput from "./shared/TextInput";
+import TextInput, { TextInputType } from "./shared/TextInput";
+import { UserDTO } from "../Types";
 
-const RegisterForm = () => {
+const RegisterForm = ({ handleOnSubmit }: Props) => {
+    const [usernameValue, setUsernameValue] = useState('');
+    const [emailValue, setEmailValue] = useState('');
+    const [passwordValue, setPasswordValue] = useState('');
+
     const username = 'Username';
-    const email = 'email';
-    const password = 'password';
+    const email = 'Email';
+    const password = 'Password';
+
+    // const handleOnChangeUsername = (enteredText: string) => {
+    //     setUsernameValue(enteredText);
+    // }
+
+    // const handleOnChangeEmail = (enteredText: string) => {
+    //     setEmailValue(enteredText);
+    // }
+
+    // const handleOnChangePassword = (enteredText: string) => {
+    //     setPasswordValue(enteredText);
+    // }
+
+    const handleFormOnSubmit = (event: any) => {
+        //event.preventDefault();
+        handleOnSubmit(event, { username: usernameValue, email: emailValue, password: passwordValue } as UserDTO);
+        // setUsernameValue('');
+        // setEmailValue('');
+        // setPasswordValue('');
+    }
 
     return (
-        <form>
+        <form method="POST" onClick={handleFormOnSubmit}>
             <fieldset>
                 <legend>Register Form</legend>
                 <p>
                     <Label name={username} htmlFor={username} />
-                    <TextInput id={username} name={username} value='' />
+                    <TextInput id={username} type={TextInputType.TEXT} name={username} value={usernameValue} handleOnChange={setUsernameValue} />
                 </p>
                 <p>
                     <Label name={email} htmlFor={email} />
-                    <TextInput id={email} name={email} value='' />
+                    <TextInput id={email} type={TextInputType.TEXT} name={email} value={emailValue} handleOnChange={setEmailValue} />
                 </p>
                 <p>
                     <Label name={password} htmlFor={password} />
-                    <TextInput id={password} name={password} value='' />
+                    <TextInput id={password} type={TextInputType.PASSWORD} name={password} value={passwordValue} handleOnChange={setPasswordValue} />
                 </p>
-
             </fieldset>
             <Button
                 type={ButtonType.SUBMIT}
-                value="Save"
+                value="Register"
             />
         </form>
     );
+}
+
+interface Props {
+    handleOnSubmit: (event: any, userDTO: UserDTO) => void;
 }
 
 export default RegisterForm;
